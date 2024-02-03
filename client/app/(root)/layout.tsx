50df39e22e34f119451e46d317ca659cf44d3e0d
Anushka gupta <anushkagupta319@gmail.com>
2024-02-03 18:45:25 +0530

    basic structure done

--- client/app/(root)/layout.tsx
+++ client/app/(root)/layout.tsx
@@ -24,15 +24,19 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <TopBar/>
-        <main>
+
+        <main className="flex flow-row">
           <LeftSidebar/>
+
           <section className="main-container">
             <div className="w-full max-w-4xl">
               {children}
             </div>
           </section>
-          <RightSidebar />
+
+          <RightSidebar/>
         </main>
+
         <Bottombar/>
         
         </body>
--- client/components/shared/Topbar.tsx
+++ client/components/shared/Topbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 //import "../globals.css";
 import Image from "next/image";
 import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
-
+import { dark } from '@clerk/themes'
 
 function TopBar() {
 
@@ -31,6 +31,7 @@ function TopBar() {
                 <OrganizationSwitcher
               
                 appearance={{
+                    baseTheme : dark,
                     elements:{
                         organizationSwitcherTrigger :"py-2 px-4"
                     }
