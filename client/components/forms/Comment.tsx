3846bd93387a0aa78d1dd5ebdd2856250229e612
Anushka gupta <anushkagupta319@gmail.com>
2024-02-28 23:04:01 +0530

    comment form

--- client/components/forms/Comment.tsx
+++ client/components/forms/Comment.tsx
@@ -10,8 +10,6 @@ const Comment = ({threadId, currentUserImg, currentUserId}: Props) => {
     return (
         <div>
             <h1 className="text-white"> Comment Form</h1>
-           
-
         </div>
     )
 
