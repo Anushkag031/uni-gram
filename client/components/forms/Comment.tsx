00edc26b98bb74f830274f153a25fcf571a27910
Anushka gupta <anushkagupta319@gmail.com>
2024-03-10 21:47:10 +0530

    comment section created

--- client/components/forms/Comment.tsx
+++ client/components/forms/Comment.tsx
@@ -1,3 +1,30 @@
+"use client"
+
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { z } from "zod"
+import { Input } from "../ui/input";
+import { updateUser } from "@/lib/actions/user.actions";
+import { usePathname, useRouter } from "next/navigation";
+
+import { CommentValidation } from "@/lib/validations/thread";
+//import { createThread } from "@/lib/actions/thread.actions";
+
+
+
 interface Props {
     threadId: string,
     currentUserImg: string,
@@ -7,12 +34,70 @@ interface Props {
 
 
 const Comment = ({threadId, currentUserImg, currentUserId}: Props) => {
+
+    const router = useRouter();
+
+    const pathname = usePathname();
+
+    const form = useForm({
+        resolver: zodResolver(CommentValidation),
+        defaultValues: {
+            thread: '',
+           
+        }
+    });
+
+
+    const onSubmit =async (values : z.infer<typeof CommentValidation>) => {
+
+        //passing necesssray values to create thread
+       /* await createThread({ 
+            text : values.thread,
+            author: userId,
+            communityId: null,
+            path: pathname,
+
+        })*/
+
+        router.push("/"); //go to home page
+
+    }
     return (
-        <div>
-            <h1 className="text-white"> Comment Form</h1>
-        </div>
+        <Form {...form}>
+            <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="mt-10 flex flex-col justify-start gap-10">
+
+                <FormField
+                    control={form.control}
+                    name="thread"
+                    render={({ field }) => (
+                        <FormItem className="flex flex-col gap-3 w-full ">
+                            <FormLabel className="text-base-semibold text-light-2">
+                               Content
+                            </FormLabel>
+                            <FormControl className="no-focus border border-dark-4 bg-dark-3 text-light-1" >
+                                <Input
+                                    type="text"
+                                    placeholder="Write a comment..."
+                                    className="no-focus text-light-1 bg-dark-3 border border-dark-4 w-full p-3 rounded-md focus:outline-none focus:border-primary-500 transition duration-300 ease-in-out"
+                                   
+                                    {...field}
+
+                                />
+                            </FormControl>
+                            <FormMessage />
+
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit" className="bg-primary-500">
+                    Post Thread
+                </Button>
+            </form>
+        </Form>
     )
 
 }
 
-export default Comment;
+export default Comment; 
