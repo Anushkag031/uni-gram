70956fb5933190da8ea8f32dbcee9949e1a2e1b8
Anushka gupta <anushkagupta319@gmail.com>
2024-03-21 12:02:02 +0530

    tabs created(imported from shadcn)

--- client/app/(root)/profile/[id]/page.tsx
+++ client/app/(root)/profile/[id]/page.tsx
@@ -1,6 +1,9 @@
 import ProfileHeader from "@/components/shared/ProfileHeader";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { profileTabs } from "@/constants";
 import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
+import Image from "next/image";
 import { redirect } from "next/navigation";
 
 
@@ -24,6 +27,43 @@ async function Page({params} : {params : {id:string}}) {
             imgUrl={userInfo.image}
             bio={userInfo.bio}
             />
+
+            {/** redendering tabs */}
+            <div className="mt-9">
+                <Tabs defaultValue="threads" className="w-full">
+                    <TabsList className="tab"> 
+                    {profileTabs.map((tab)=> ( //coming from constants index.js (all are shadcn features)
+                        <TabsTrigger key={tab.label} value={tab.value} className="tab">
+                            <Image
+                            src={tab.icon}
+                            alt={tab.label}
+                            width={24}
+                            height={24}
+                            className="object-contain"
+                            />
+                            <p className="max-sm:hidden">{tab.label}</p>
+
+                            {tab.label==='Threads' && (
+                                <p className="ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-light-2">
+                                    {userInfo?.threads?.length} {/** display number of threads user have */}
+                                </p>
+                            )}
+                        </TabsTrigger>
+                    ))
+                    }
+                    </TabsList>
+                    {/*{profileTabs.map((tab)=> (
+                        <TabsContent key={`content-${tab.label}`} value={tab.value} className="w-full text-light-1">
+                            <ThreadsTab
+                            currentUserId={user.id}
+                            accountId={userInfo.id}
+                            accountType="User"
+                            />
+                        </TabsContent>
+                    ))}*/}
+                </Tabs>
+
+            </div>
         </section>
     )
 }
--- client/components/shared/ProfileHeader.tsx
+++ client/components/shared/ProfileHeader.tsx
@@ -35,8 +35,13 @@ const ProfileHeader = ({
                         <p className="text-base-medium text-gray-1">@{username}</p>
                     </div>
                 </div>    
-                {/** todo : community */}
             </div>
+                {/** todo : community */}
+
+                <p className="mt-6 max-w-lg text-base-regular text-light-2">{bio}</p>
+
+                <div className="mt-12 h-0.5 w-full bg-dark-2"/>
+            
         </div>
     )
 }
